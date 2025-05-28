7bc9aab29310243c2bb850fb9352936f0ed11ff2
Grishechko Dmitriy <mitrio78@gmail.com>
2025-05-28 18:37:27 +0300

    Initial Commit

---
+++ FitLink/FitLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitLinkApp.swift
+//  FitLink
+//
+//  Created by Дмитрий Гришечко on 28.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
